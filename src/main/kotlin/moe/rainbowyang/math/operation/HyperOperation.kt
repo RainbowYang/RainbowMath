d388e8ec20de3be86c8c3c18789b7d54bd139cdc
Rainbow Yang <q1041410345@gmail.com>
2018-09-30 17:11:47 +0800

    重构operation

--- src/main/kotlin/moe/rainbowyang/math/operation/HyperOperation.kt
+++ src/main/kotlin/moe/rainbowyang/math/operation/HyperOperation.kt
@@ -4,65 +4,16 @@ package moe.rainbowyang.math.operation
  * 超运算
  * @author Rainbow Yang
  */
-interface HyperOperation<This> : HyperOperation1<This>, HyperOperation2<This> {
-    interface Math<This : HyperOperation<This>> : HyperOperation1.Math<This>, HyperOperation2.Math<This>
-    interface WithOther<This : HyperOperation.WithOther<This, Other>, Other> :
-            HyperOperation1.WithOther<This, Other>, HyperOperation2.WithOther<This, Other> {
-        interface Math<This : HyperOperation.WithOther<This, Other>, Other> :
-                HyperOperation1.WithOther.Math<This, Other>, HyperOperation2.WithOther.Math<This, Other>
-    }
-}
+interface HyperOperation<This> : HyperOperation1<This>, HyperOperation2<This>
 
 interface HyperOperation1<This> : Operation<This> {
-
     operator fun plus(other: This): This
     operator fun minus(other: This): This = plus(unaryMinus())
     operator fun unaryMinus(): This
-
-    operator fun plus(other: Number) = plus(other.asThis())
-    operator fun minus(other: Number) = minus(other.asThis())
-
-    interface Math<This : HyperOperation1<This>> : Operation.Math<This> {
-        operator fun Number.plus(other: This) = other.plus(this)
-        operator fun Number.minus(other: This) = other.minus(this)
-    }
-
-    interface WithOther<This : HyperOperation1.WithOther<This, Other>, Other> :
-            Operation.WithOther<This, Other>, HyperOperation1<This> {
-
-        operator fun plus(other: Other) = plus(other.asThis())
-        operator fun minus(other: Other) = minus(other.asThis())
-
-        interface Math<This : HyperOperation1.WithOther<This, Other>, Other> : Operation.Math<This> {
-            operator fun Other.plus(other: This) = other.plus(this)
-            operator fun Other.minus(other: This) = other.minus(this)
-        }
-    }
 }
 
 interface HyperOperation2<This> : Operation<This> {
-
     operator fun times(other: This): This
     operator fun div(other: This): This = times(reciprocal())
     /*hold*/ fun reciprocal(): This
-
-    operator fun times(other: Number) = times(other.asThis())
-    operator fun div(other: Number) = div(other.asThis())
-
-    interface Math<This : HyperOperation2<This>> : Operation.Math<This> {
-        operator fun Number.times(other: This) = other.times(this)
-        operator fun Number.div(other: This) = other.div(this)
-    }
-
-    interface WithOther<This : HyperOperation2.WithOther<This, Other>, Other> :
-            Operation.WithOther<This, Other>, HyperOperation2<This> {
-
-        interface Math<This : HyperOperation2.WithOther<This, Other>, Other> : Operation.Math<This> {
-            operator fun Other.times(other: This) = other.times(this)
-            operator fun Other.div(other: This) = other.div(this)
-        }
-
-        operator fun times(other: Other) = times(other.asThis())
-        operator fun div(other: Other) = div(other.asThis())
-    }
 }
--- src/main/kotlin/moe/rainbowyang/math/operation/Operation.kt
+++ src/main/kotlin/moe/rainbowyang/math/operation/Operation.kt
@@ -5,23 +5,5 @@ package moe.rainbowyang.math.operation
  * @author Rainbow Yang
  */
 interface Operation<This> {
-    fun Number.asThis(): This
 
-    /**
-     * 此接口用于扩展
-     * 须由单例实现，提供静态方法
-     */
-    interface Math<This : Operation<This>> {
-        val ZERO: This
-        fun Number.asThis(): This = with(ZERO) { asThis() }
-    }
-
-    interface WithOther<This, Other> : Operation<This> {
-        fun Other.asThis(): This
-
-        interface Math<This, Other> : Operation.Math<This>
-                where This : WithOther<This, Other>, This : Operation<This> {
-            fun Other.asThis(): This = with(ZERO) { asThis() }
-        }
-    }
 }

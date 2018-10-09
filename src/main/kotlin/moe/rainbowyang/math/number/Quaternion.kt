f5d18c0ed6db9b472336bb7f95969946fc7d4b0e
Rainbow Yang <q1041410345@gmail.com>
2018-10-09 17:44:06 +0800

    为Quaternion添加LogAndExp

--- src/main/kotlin/moe/rainbowyang/math/number/Quaternion.kt
+++ src/main/kotlin/moe/rainbowyang/math/number/Quaternion.kt
@@ -1,9 +1,18 @@
 package moe.rainbowyang.math.number
 
+import moe.rainbowyang.math.number.Quaternion.Math.arg
 import moe.rainbowyang.math.number.Quaternion.Math.asQuaternion
+import moe.rainbowyang.math.number.Quaternion.Math.sgn
 import moe.rainbowyang.math.number.RealNumber.Math.lengthOf
+import moe.rainbowyang.math.number.RealNumber.Math.cos
+import moe.rainbowyang.math.number.RealNumber.Math.exp
+import moe.rainbowyang.math.number.RealNumber.Math.asReal
+import moe.rainbowyang.math.number.RealNumber.Math.ln
+import moe.rainbowyang.math.number.RealNumber.Math.sin
 import moe.rainbowyang.math.number.RealNumber.Math.sumOfSquare
 import moe.rainbowyang.math.operation.HyperOperation
+import moe.rainbowyang.math.operation.LogAndExp
+import kotlin.math.acos
 
 /**
  *  四元数（a+bi+cj+dk）
@@ -11,9 +20,12 @@ import moe.rainbowyang.math.operation.HyperOperation
  */
 data class Quaternion(val a: RealNumber, val b: RealNumber, val c: RealNumber, val d: RealNumber) :
         Number,
-        HyperOperation<Quaternion> {
+        HyperOperation<Quaternion>,
+        LogAndExp<Quaternion> {
 
     val modulus = lengthOf(a, b, c, d)
+    val scalar = a
+    val vecter = Quaternion(RealNumber.ZERO, b, c, d)
 
     override operator fun plus(other: Quaternion) =
             Quaternion(a + other.a, b + other.b, c + other.c, d + other.d)
@@ -29,6 +41,9 @@ data class Quaternion(val a: RealNumber, val b: RealNumber, val c: RealNumber, v
 
     override fun reciprocal() = this.conjugate() / sumOfSquare(a, b, c, d).asQuaternion()
 
+    override fun exp(): Quaternion = exp(a).asQuaternion() * (cos(vecter.modulus).asQuaternion() + sgn(vecter) * sin(vecter.modulus).asQuaternion())
+    override fun ln(): Quaternion = ln(modulus).asQuaternion() + sgn(vecter) * arg(this).asQuaternion()
+
     /** 共轭 */
     fun conjugate() = Quaternion(a, -b, -c, -d)
 
@@ -38,6 +53,17 @@ data class Quaternion(val a: RealNumber, val b: RealNumber, val c: RealNumber, v
             "${if (d >= RealNumber.ZERO) "+" else ""}${d}k"
 
     object Math {
+        fun kotlin.Number.asQuaternion() = Quaternion(this.asReal(), RealNumber.ZERO, RealNumber.ZERO, RealNumber.ZERO)
         fun RealNumber.asQuaternion() = Quaternion(this, RealNumber.ZERO, RealNumber.ZERO, RealNumber.ZERO)
+        fun ComplexNumber.asQuaternion() = Quaternion(real, imag, RealNumber.ZERO, RealNumber.ZERO)
+
+        fun inner(p: Quaternion, q: Quaternion) = q.a * p.a + q.b * p.b + q.c * p.c + q.d * p.d
+        fun outer(p: Quaternion, q: Quaternion) = (p.conjugate() * q - q.conjugate() * p) / 2.asQuaternion()
+        fun even(p: Quaternion, q: Quaternion) = (p * q + q * p) / 2.asQuaternion()
+        fun odd(p: Quaternion, q: Quaternion) = (p * q - q * p) / 2.asQuaternion()
+
+        fun sgn(p: Quaternion) = p / p.modulus.asQuaternion()
+        fun arg(p: Quaternion) = acos((p.a / p.modulus).value)
+
     }
 }
